86c1e1af693e146f2c4c7d61832a812b1866b235
Jake West <cakeeatsgirl@gmail.com>
2020-10-03 18:02:43 -0400

    Add tuner reducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -5,9 +5,11 @@ import thunk from "redux-thunk";
 import game from "./reducers/gameReducer";
 import ui from "./reducers/uiReducer";
 import error from "./reducers/errorReducer.js";
+import tuner from "./reducers/tunerReducer.js";
 
 const rootReducer = combineReducers({
   game,
+  tuner,
   ui,
   error,
 });
